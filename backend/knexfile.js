4af4420cc052687fe135477284a38cf7a8f7f724
michael-leibolt <mike.leibolt1.1@gmail.com>
2025-09-05 16:27:36 -0400

    intial setup complete

---
+++ backend/knexfile.js
@@ -0,0 +1,17 @@
+// Update with your config settings.
+require('dotenv').config()
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+  development: {
+    client: "pg",
+    connection: {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+    }
+  }
+};
---
+++ backend/migrations/20250905201309_readiness_snapshots.js
@@ -0,0 +1,17 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable('readiness_snapshots', table => {
+
+  })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExits('readiness_snapshots')
+};
