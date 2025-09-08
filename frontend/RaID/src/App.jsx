4e2ec74d0beb04b7d24b580d16a2d0d0ffbf3f64
waitholdthis <parker.tootill@gmail.com>
2025-09-08 16:12:52 -0400

    Try Again

--- frontend/RaID/src/App.jsx
+++ frontend/RaID/src/App.jsx
@@ -28,4 +28,21 @@ function App() {
   )
 }
 
-export default App
+
+
+import { Routes, Route } from "react-router-dom";
+import AppLayout from "./AppLayout";
+import Dashboard from "./routes/Dashboard";
+import ThreeFiftyOne from "./routes/ThreeFiftyOne";
+
+export default function App() {
+  return (
+    <Routes>
+      <Route element={<AppLayout />}>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/350-1" element={<ThreeFiftyOne />} />
+      </Route>
+    </Routes>
+  );
+}
