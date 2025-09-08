f2db305fa3cd53a718e8ac4f5a5397b97947c760
michael-leibolt <mike.leibolt1.1@gmail.com>
2025-09-08 14:33:33 -0400

    'Partial Seed Data Complete'

--- backend/seeds/03_units.js
+++ backend/seeds/03_units.js
@@ -1,7 +1,18 @@
 /**
  * @param { import("knex").Knex } knex
- * @returns { Promise<void> } 
+ * @returns { Promise<void> }
  */
+
+  let unitNum = Math.floor(Math.random() * (300 - 1 + 1)) + 1
+  let nameNum = Math.floor(Math.random() * 5)
+
+let nameType = [
+  {name: 'Engineer Co'},
+  {name: 'Signal Co'},
+  {name: 'Infantry Co'},
+  {name: 'Military Intelligence Co'},
+  {name: 'Forward Support Co'}
+]
 exports.seed = async function(knex) {
   // Deletes ALL existing entries
   await knex('table_name').del()
