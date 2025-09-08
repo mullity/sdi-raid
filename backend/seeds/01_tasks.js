381b72043dac46db5ad794a1eb2169457e7f91c3
michael-leibolt <mike.leibolt1.1@gmail.com>
2025-09-08 14:33:33 -0400

    'Partial Seed Data Complete'

--- backend/seeds/01_tasks.js
+++ backend/seeds/01_tasks.js
@@ -1,13 +1,30 @@
 /**
  * @param { import("knex").Knex } knex
- * @returns { Promise<void> } 
+ * @returns { Promise<void> }
  */
+
+const { faker } = require('@faker-js/faker');
+
+let data = []
+let faked = 10
+let trainingLevel = [
+  {name: 'individual'},
+  {name: 'crew'},
+  {name: 'collective'}
+]
+
+for(let i = 0; i < faked; i++){
+  let trainingNum = Math.floor(Math.random() * 3)
+
+  data.push({id: i, name: ``, level: `${trainingLevel[trainingNum].name}`})
+
+  console.log(i)
+  console.log(``)
+  console.log(`${trainingLevel[trainingNum].name}`)
+}
+
 exports.seed = async function(knex) {
   // Deletes ALL existing entries
-  await knex('table_name').del()
-  await knex('table_name').insert([
-    {id: 1, colName: 'rowValue1'},
-    {id: 2, colName: 'rowValue2'},
-    {id: 3, colName: 'rowValue3'}
-  ]);
+  await knex('tasks').del()
+  await knex('tasks').insert(data);
 };
