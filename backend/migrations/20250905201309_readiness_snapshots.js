9e41704ff5c72da51b254d9e7a1e1817e5f0dd43
michael-leibolt <mike.leibolt1.1@gmail.com>
2025-09-09 13:12:39 -0400

    updated readiness snaps migration/seed, updated users migration/seed,

--- backend/migrations/20250905201309_readiness_snapshots.js
+++ backend/migrations/20250905201309_readiness_snapshots.js
@@ -4,12 +4,19 @@
  */
 exports.up = function(knex) {
   return knex.schema.createTable('readiness_snapshots', table => {
-    table.increments('id').primary()
-    table.integer('unit_id').notNullable()
-    table.date('snapshot_date').notNullable()
-    table.decimal('overall_score', 4, 2)
+    table.increments('id').primary();
+    table.integer('user_id').notNullable();
+    table.integer('unit_id').notNullable();
+    table.timestamp('last_login').defaultTo(knex.fn.now()).notNullable()
+    table.timestamp('last_logout');
+    table.decimal('overall_score', 4, 2);
+    table.string('personnel');
+    table.string('training');
+    table.string('maintenance');
+    table.string('medical');
 
-    table.foreign('unit_id').references('units.id')
+    table.foreign('unit_id').references('units.id');
+    table.foreign('user_id').references('users.id').deferrable('deferred');
 
   })
 };
@@ -19,5 +26,11 @@ exports.up = function(knex) {
  * @returns { Promise<void> }
  */
 exports.down = function(knex) {
-  return knex.schema.dropTableIfExists('readiness_snapshots')
+  return knex.schema.alterTable('readiness_snapshots', table => {
+    table.dropForeign('user_id');
+    table.dropForeign('unit_id');
+  })
+  .then( () => {
+    return knex.schema.dropTableIfExists('readiness_snapshots');
+  })
 };
