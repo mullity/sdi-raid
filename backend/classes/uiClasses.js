eb0833dd6d600572d61124d06502bc6cabfea3a9
mullity <mullis.tyler.work@gmail.com>
2025-09-12 10:44:47 -0600

    training snapshot to parent class datacard

--- backend/classes/uiClasses.js
+++ backend/classes/uiClasses.js
@@ -2,16 +2,16 @@ class UiCard {
     /**
    * Creates a modal istance.
    * @param {string} id - string name of the modal being supplied
-   * @param {number} percent - percentage of satisfactory items, expressed as a whole number
+   * @param {number} value - percentage of satisfactory items, expressed as a whole number
    * @param {object} data - object of key:value pairs for other data, depending on modal type
    * @property {string} id - string name of the modal being supplied (also documented here for clarity).
-   * @property {number} percent - percentage of satisfactory items, expressed as a whole number (also documented here for clarity).
+   * @property {number} value - percentage of satisfactory items, expressed as a whole number (also documented here for clarity).
    * @property {object} data - object of key:value pairs for other verbose data, depending on modal type
    */
 
-    constructor(id, percent, data) {
+    constructor(id, value, data) {
         this.id = id
-        this.percent = percent
+        this.value = value
         if (data) {
             this.data = data
         }
@@ -24,15 +24,25 @@ class Snapshot extends UiCard {
         super()
     }
 
+    generateDataResponse(valueType = 'percent', verboseData = {}) {
+        const dataResponse = {
+            'id': this.id,
+            'value': this.value,
+            'valueType': valuetype,
+            'data': verboseData
+        }
+        return dataResponse
+    }
+
 }
 
 class TrainingSnapshot extends Snapshot {
 
     constructor() {
         super()
-        this.id="training"
-        this.percent=0
-        this.data={}
+        this.id = "training"
+        this.value = 0
+        this.data = {}
         this.qualified = 0
         this.notQualified = 0
         this.tasks = []
@@ -46,40 +56,34 @@ class TrainingSnapshot extends Snapshot {
     }
 
     generateCard(verbose) {
-        for(let join of this.joined){
-            if(join.assigned_unit_id == Number(this.unit)){
-            this.tasks.push(join)
+        for (let join of this.joined) {
+            if (join.assigned_unit_id == Number(this.unit)) {
+                this.tasks.push(join)
             }
         }
 
-        for(let task of this.tasks){
-            if(task.status == "qualified" ) {
-            this.qualified++
-            } else if (task.status == "not_qualified"){
-            this.notQualified++
+        for (let task of this.tasks) {
+            if (task.status == "qualified") {
+                this.qualified++
+            } else if (task.status == "not_qualified") {
+                this.notQualified++
             }
         }
 
         let tasksT = this.tasks.length
-        let tasksPercent = Number(this.qualified/tasksT) * 100
+        this.value = Number(this.qualified / tasksT) * 100
         let output
 
-        if(verbose === "true"){
-            output =
-            {id: 'Training',
-            percent: tasksPercent,
-            data: {
-            total: this.tasks.length,
-            qualified: this.qualified,
-            notqualified: this.notQualified
-            }}
+        if (verbose === "true") {
+            let snapData = {
+                total: this.tasks.length,
+                qualified: this.qualified,
+                notqualified: this.notQualified
+            }
+            return this.generateDataResponse('percent', snapData)
         } else {
-            output =
-            {id: 'Training',
-            percent: tasksPercent}
+            return this.generateDataResponse('percent', snapData)
         }
-
-        return output;
     }
 
 }
@@ -88,9 +92,9 @@ class MedicalSnapshot extends Snapshot {
 
     constructor() {
         super()
-        this.id="medical"
-        this.percent=0
-        this.data={}
+        this.id = "medical"
+        this.value = 0
+        this.data = {}
         this.green = 0
         this.red = 0
         this.amber = 0
@@ -104,41 +108,44 @@ class MedicalSnapshot extends Snapshot {
     }
 
     generateCard(verbose) {
-        for(let troop of this.troops){
-            if(troop.medical_status == "5"){
-            this.green++
+        for (let troop of this.troops) {
+            if (troop.medical_status == "5") {
+                this.green++
             }
-            if(troop.medical_status == "6"){
-            this.amber++
+            if (troop.medical_status == "6") {
+                this.amber++
             }
-            if (troop.medical_status == "7"){
-            this.red++
+            if (troop.medical_status == "7") {
+                this.red++
             }
         }
 
         let troopT = this.troops.length
-        let troopPercent = Number(this.green/troopT) * 100
+        let troopPercent = Number(this.green / troopT) * 100
         let output
 
 
 
 
-            if(verbose === "true"){
+        if (verbose === "true") {
             output =
-            {id: "Medical",
-            percent: troopPercent,
-            data: {
-                total: this.troops.length,
-                green: this.green,
-                amber: this.amber,
-                red: this.red,
+            {
+                id: "Medical",
+                value: troopPercent,
+                data: {
+                    total: this.troops.length,
+                    green: this.green,
+                    amber: this.amber,
+                    red: this.red,
 
-            }}
-            } else {
-                output =
-                {id: "Medical", percent: troopPercent
                 }
             }
+        } else {
+            output =
+            {
+                id: "Medical", value: troopPercent
+            }
+        }
 
         return output;
     }
@@ -148,9 +155,9 @@ class PersonnelSnapshot extends Snapshot {
 
     constructor() {
         super()
-        this.id="personnel"
-        this.percent=0
-        this.data={}
+        this.id = "personnel"
+        this.value = 0
+        this.data = {}
         this.deployable = 0
         this.nonDeployable = 0
         this.troops = []
@@ -163,34 +170,36 @@ class PersonnelSnapshot extends Snapshot {
     }
 
     generateCard(verbose) {
-        for(let troop of this.troops){
-            if(troop.deployable_status == "5" || troop.deployable_status == "6" ) {
-            this.deployable++
-            } else if (troop.deployable_status == "7"){
-            this.nonDeployable++
+        for (let troop of this.troops) {
+            if (troop.deployable_status == "5" || troop.deployable_status == "6") {
+                this.deployable++
+            } else if (troop.deployable_status == "7") {
+                this.nonDeployable++
             }
         }
 
         let troopT = this.troops.length
-        let troopPercent = Number(this.deployable/troopT) * 100
+        let troopPercent = Number(this.deployable / troopT) * 100
         let output
 
 
 
 
-            if(verbose === "true"){
+        if (verbose === "true") {
             output =
-            {id: "Personnel",
-            percent: troopPercent,
-            data: {
-                total: this.troops.length,
-                deployable: this.deployable,
-                nondeployable: this.nonDeployable
-            }}
-            } else {
-            output =
-            {id: "Personnel", percent: troopPercent}
+            {
+                id: "Personnel",
+                value: troopPercent,
+                data: {
+                    total: this.troops.length,
+                    deployable: this.deployable,
+                    nondeployable: this.nonDeployable
+                }
             }
+        } else {
+            output =
+                { id: "Personnel", value: troopPercent }
+        }
 
         return output;
     }
@@ -200,9 +209,9 @@ class VehicleSnapshot extends Snapshot {
 
     constructor() {
         super()
-        this.id="vehicle"
-        this.percent=0
-        this.data={}
+        this.id = "vehicle"
+        this.value = 0
+        this.data = {}
         this.fmc = 0
         this.pmc = 0
         this.nmc = 0
@@ -255,7 +264,7 @@ class VehicleSnapshot extends Snapshot {
             output =
             {
                 id: "Equipment",
-                percent: vicPercent,
+                value: vicPercent,
                 data: {
                     total: this.vics.length,
                     FMC: this.fmc,
@@ -266,7 +275,7 @@ class VehicleSnapshot extends Snapshot {
         } else {
             output = {
                 id: "Equipment",
-                percent: vicPercent,
+                value: vicPercent,
             }
         }
 
@@ -289,10 +298,10 @@ class Modal extends UiCard {
 
 
 module.exports = {
-    VehicleSnapshot:VehicleSnapshot,
-    TrainingSnapshot:TrainingSnapshot,
-    PersonnelSnapshot:PersonnelSnapshot,
-    MedicalSnapshot:MedicalSnapshot,
+    VehicleSnapshot: VehicleSnapshot,
+    TrainingSnapshot: TrainingSnapshot,
+    PersonnelSnapshot: PersonnelSnapshot,
+    MedicalSnapshot: MedicalSnapshot,
     UiCard: UiCard,
     Modal: Modal
 }
