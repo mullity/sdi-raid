f1a66663d10684f96520c0881339655284bde12d
michael-leibolt <mike.leibolt1.1@gmail.com>
2025-09-16 14:39:23 -0400

    updated indexOutput to return the url it hit as well

--- backend/indexOutput.js
+++ backend/indexOutput.js
@@ -1,10 +1,11 @@
 fetch('http://localhost:3001/snapshot?unit=1')
   .then(res => res.json())
   .then(data=> {
+    let url = 'http://localhost:3001/snapshot?unit=1'
     let fetchName = 'GET: SNAPSHOT'
     let vars = 'required query params: unit(number) \noptional query params: verbose(true/false as string) *Not implemented. No effect on return at this time*'
     console.log(
-      `${fetchName}\n${vars}\n`,
+      `${fetchName}\n${vars}\nURL:${url}\n`,
     )
     console.dir(data, { depth: null });
   })
@@ -12,10 +13,11 @@ fetch('http://localhost:3001/snapshot?unit=1')
 fetch('http://localhost:3001/kpi?unit=8&verbose=true&personnelReadinessScore=true&equipmentReadinessScore=true&trainingReadinessScore=true&medicalReadinessScore=true')
   .then(res => res.json())
   .then(data=> {
+    let url = 'http://localhost:3001/kpi?unit=8&verbose=true&personnelReadinessScore=true&equipmentReadinessScore=true&trainingReadinessScore=true&medicalReadinessScore=true'
     let fetchName = 'GET: KPI'
     let vars = 'required query params: unit(number) \noptional query params: verbose(true/false as string), personnelReadinessScore(true/false as string), equipmentReadinessScore(true/false as string), trainingReadinessScore(true/false as string), medicalReadinessScore(true/false as string)'
     console.log(
-      `${fetchName}\n${vars}\n`,
+      `${fetchName}\n${vars}\nURL:${url}\n`,
     )
     console.dir(data, { depth: null });
   })
@@ -23,10 +25,11 @@ fetch('http://localhost:3001/kpi?unit=8&verbose=true&personnelReadinessScore=tru
 fetch('http://localhost:3001/kpi?unit=1&personnelReadinessScore=true')
   .then(res => res.json())
   .then(data=> {
+    let url = 'http://localhost:3001/kpi?unit=1&personnelReadinessScore=true'
     let fetchName = 'GET: KPI'
     let vars = 'required query params: unit(number) \noptional query params: verbose(true/false as string), personnelReadinessScore(true/false as string), equipmentReadinessScore(true/false as string), trainingReadinessScore(true/false as string), medicalReadinessScore(true/false as string)'
     console.log(
-      `${fetchName}\n${vars}\n`,
+      `${fetchName}\n${vars}\nURL:${url}\n`,
     )
     console.dir(data, { depth: null });
   })
@@ -34,10 +37,11 @@ fetch('http://localhost:3001/kpi?unit=1&personnelReadinessScore=true')
 fetch('http://localhost:3001/priority?unit=1')
   .then(res => res.json())
   .then(data=> {
+    let url = 'http://localhost:3001/priority?unit=1'
     let fetchName = 'GET: PRIORITY'
     let vars = 'Same as snapshot, but ordered by value lowest first \nrequired query params: unit(number) \noptional query params: verbose(true/false as string) *Not implemented. No effect on return at this time*'
     console.log(
-      `${fetchName}\n${vars}\n`,
+      `${fetchName}\n${vars}\nURL:${url}\n`,
     )
     console.dir(data, { depth: null });
   })
@@ -45,10 +49,11 @@ fetch('http://localhost:3001/priority?unit=1')
 fetch('http://localhost:3001/modal?unit=8&vicModalValue=true&deploymentModalValue=true&verbose=true')
   .then(res => res.json())
   .then(data=> {
+    let url = 'http://localhost:3001/modal?unit=8&vicModalValue=true&deploymentModalValue=true&verbose=true'
     let fetchName = 'GET: MODAL'
     let vars = 'Other params not implemented at this time, will be by EOD tuesday following same format as below \nrrequired query params: unit(number) \noptional query params: verbose(true/false as string), vicModalValue(true/false as string), deploymentModalValue(true/false as string), crewModalValue(true/false as string), medModalValue(true/false as string), weaponModalValue(true/false as string)'
     console.log(
-      `${fetchName}\n${vars}\n`,
+      `${fetchName}\n${vars}\nURL:${url}\n`,
     )
     console.dir(data, { depth: null });
   })
@@ -56,10 +61,11 @@ fetch('http://localhost:3001/modal?unit=8&vicModalValue=true&deploymentModalValu
 fetch('http://localhost:3001/modal?unit=8&vicModalValue=true&deploymentModalValue=true')
   .then(res => res.json())
   .then(data=> {
+    let url = 'http://localhost:3001/modal?unit=8&vicModalValue=true&deploymentModalValue=true'
     let fetchName = 'GET: MODAL'
     let vars = 'Other params not implemented at this time, will be by EOD tuesday following same format as below \nrrequired query params: unit(number) \noptional query params: verbose(true/false as string), vicModalValue(true/false as string), deploymentModalValue(true/false as string), crewModalValue(true/false as string), medModalValue(true/false as string), weaponModalValue(true/false as string)'
     console.log(
-      `${fetchName}\n${vars}\n`,
+      `${fetchName}\n${vars}\nURL:${url}\n`,
     )
     console.dir(data, { depth: null });
   })
@@ -67,10 +73,11 @@ fetch('http://localhost:3001/modal?unit=8&vicModalValue=true&deploymentModalValu
 fetch('http://localhost:3001/users/uic?uic=WAMZAA')
   .then(res => res.json())
   .then(data=> {
+    let url = 'http://localhost:3001/users/uic?uic=WAMZAA'
     let fetchName = 'GET: USERS/UIC'
     let vars = '\nrequired query params: unit(number or all caps UIC)'
     console.log(
-      `${fetchName}\n${vars}\n`,
+      `${fetchName}\n${vars}\nURL:${url}\n`,
     )
     console.dir(data, { depth: null });
   })
