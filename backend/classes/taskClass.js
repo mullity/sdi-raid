6cfd69b64476fc8f04b70df4bb5fff04f138097c
mullity <mullis.tyler.work@gmail.com>
2025-09-16 10:09:57 -0600

    Task class init

---
+++ backend/classes/taskClass.js
@@ -0,0 +1,67 @@
+class CatsRollup {
+    /**
+   * Creates a modal istance.
+   * @param {string} id - string name of the modal being supplied
+   * @param {number} value - percentage of satisfactory items, expressed as a whole number
+   * @param {object} data - object of key:value pairs for other data, depending on modal type
+   * @property {string} id - string name of the modal being supplied (also documented here for clarity).
+   * @property {number} value - percentage of satisfactory items, expressed as a whole number (also documented here for clarity).
+   * @property {object} data - object of key:value pairs for other verbose data, depending on modal type
+   */
+
+    constructor(id, name, data) {
+        this.id = id
+        this.name = value
+        if (data) {
+            this.data = data
+        }
+    }
+
+
+}
+
+
+class TaskSet {
+    /**
+   * Creates a modal istance.
+   * @param {string} id - string name of the modal being supplied
+   * @param {number} value - percentage of satisfactory items, expressed as a whole number
+   * @param {object} data - object of key:value pairs for other data, depending on modal type
+   * @property {string} id - string name of the modal being supplied (also documented here for clarity).
+   * @property {number} value - percentage of satisfactory items, expressed as a whole number (also documented here for clarity).
+   * @property {object} data - object of key:value pairs for other verbose data, depending on modal type
+   */
+
+    constructor(id, name, data) {
+        this.id = id
+        this.name = value
+        if (data) {
+            this.data = data
+        }
+    }
+
+
+}
+
+class Event {
+    /**
+   * Creates a modal istance.
+   * @param {string} id - string name of the modal being supplied
+   * @param {number} value - percentage of satisfactory items, expressed as a whole number
+   * @param {object} data - object of key:value pairs for other data, depending on modal type
+   * @property {string} id - string name of the modal being supplied (also documented here for clarity).
+   * @property {number} value - percentage of satisfactory items, expressed as a whole number (also documented here for clarity).
+   * @property {object} data - object of key:value pairs for other verbose data, depending on modal type
+   */
+
+    constructor(id, name, data) {
+        this.id = id
+        this.name = value
+        if (data) {
+            this.data = data
+        }
+    }
+
+
+}
+
