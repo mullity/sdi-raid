490ca4be78b896c17c308a0f2ea399dd2e74960b
mullity <mullis.tyler.work@gmail.com>
2025-09-17 09:05:27 -0600

    fixed again

--- backend/index.js
+++ backend/index.js
@@ -32,7 +32,7 @@ const secretKey = process.env.SECRET_KEY;
 const app = express();
 app.use(
   cors({
-    origin: ["http://127.0.0.1:5173","http://localhost:3001"],
+    origin: ["http://127.0.0.1:5173","http://localhost:5173"],
     credentials: true,
   })
 );
