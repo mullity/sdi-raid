f5eb49463d92fa3f25d30dec68a77242b8a1b909
mullity <mullis.tyler.work@gmail.com>
2025-09-18 11:02:37 -0600

    start point for task home

--- frontend/src/routes/TaskHome.jsx
+++ frontend/src/routes/TaskHome.jsx
@@ -5,18 +5,49 @@ import { useParams, useNavigate } from "react-router-dom";
 
 
 export default function TaskHome({ inputTask = '17-CW-5969' }) {
-  const navigate = useNavigate()
+    const navigate = useNavigate()
+    //TEMP Hardcode
+    const METL = [
+        {
+            "number": "05-CO-3000",
+            "title": "Coordinate Survivability Operations"
+        },
+        {
+            "number": "05-CO-2012",
+            "title": "Provide Engineer Support to Countermobility"
+        },
+        {
+            "number": "05-CO-1025",
+            "title": "Provide Engineer Support for Mobility Operations"
+        },
+        {
+            "number": "05-CO-3000",
+            "title": "Conduct Reconnaissance Planning"
+        },
+        {
+            "number": "05-CO-1700",
+            "title": "Provide Engineer Support to Explosive Hazards (EHs) Clearing Operations"
+        },
+        {
+            "number": "55-CO-4830",
+            "title": "Conduct Expeditionary Deployment Operations"
+        }
+    ]
 
-  return (
-      <div className="equip-page">
-        <header className="equip-header">
-          <div>
-            <h1>METL Tasks</h1>
-            <p className="equip-sub">Mission Essential Tasks for UIC: WAMZB0</p>
-            
-          </div>
-        </header>
+    return (
+        <div className="equip-page">
+            <header className="equip-header">
+                <div>
+                    <h1>METL Tasks</h1>
+                    <p className="equip-sub">Mission Essential Tasks for UIC: WAMZB0</p>
+                    {METL.map((task, index) => (<li key={index} >
+                        <a onClick={() => navigate(`/task-viewer/${task.number}`)} style={{ cursor: 'pointer' }}>{task.number} - {task.title}</a>
+                    </li>))
+                    }
 
-      </div>
+                </div>
+            </header>
+
+        </div>
     );
-  }
+}
