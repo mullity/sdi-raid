54c4607a6f9639bcb48ffb4b36f0534743ff4b81
mullity <mullis.tyler.work@gmail.com>
2025-09-19 10:38:14 -0600

    added comment

--- frontend/src/components/LeaderHub.jsx
+++ frontend/src/components/LeaderHub.jsx
@@ -49,7 +49,7 @@ function LeaderHub({ selectedUnit }) {
       id: datum.id,
       title: datum.data.title,
       description: datum.data.description,
-      percentage: isNaN(Math.round(datum.data.percentage * 100) /100) ? 69 : Math.round(datum.data.percentage * 100)/100,
+      percentage: isNaN(Math.round(datum.data.percentage * 100) /100) ? 69 : Math.round(datum.data.percentage * 100)/100, //TODO - TEMP FIX FOR NAN
       status: status,
       data: datum.data
     })
